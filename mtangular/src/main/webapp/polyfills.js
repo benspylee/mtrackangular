41bb1ffb8d033d69d7e82462a466ad9a005caad1
DESKTOP-QL7PG73\Naduvan <benspylee@hotmail.com>
2018-07-25 23:35:13 +0530

    added json fix

--- mtangular/src/main/webapp/polyfills.js
+++ mtangular/src/main/webapp/polyfills.js
@@ -5472,7 +5472,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Barath-PC\ChargeZone\Mtrack\mtrackangular\mtangular\angular-ui\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Naduvan\git\mtrackangular\mtangular\angular-ui\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
